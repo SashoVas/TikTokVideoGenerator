90f9ada618e27da3ae09d0c46f2435d9896f955c
SashoVas <98760930+SashoVas@users.noreply.github.com>
2024-09-16 20:02:08 +0300

    Mergeing crate_image_with_text function with create_image_with_text_transparent

--- subtitles_generator.py
+++ subtitles_generator.py
@@ -7,63 +7,15 @@ class SubtitlesGenerator:
         self.max_width = max_width
         self.padding = padding
 
-    def create_image_with_text_transparent(self, text, output_path='images/output_image.png'):
-        # Initialize ImageDraw to determine text size
-        font = ImageFont.truetype("arial.ttf", self.font_size)
-        # Transparent dummy image
-        dummy_image = Image.new('RGBA', (1, 1), (0, 0, 0, 0))
-        draw = ImageDraw.Draw(dummy_image)
-
-        # Text wrapping function
-        def wrap_text(text, font, max_width):
-            lines = []
-            words = text.split(' ')
-            while words:
-                line = ''
-                while words and draw.textbbox((0, 0), line + words[0], font=font)[2] <= max_width:
-                    line += (words.pop(0) + ' ')
-                lines.append(line)
-            return lines
-
-        # Wrap text to fit within the specified maximum width
-        wrapped_text = wrap_text(text, font, self.max_width)
-
-        # Calculate image dimensions based on wrapped text
-        max_line_width = max(draw.textbbox((0, 0), line, font=font)[
-                             2] for line in wrapped_text)
-        total_text_height = sum(draw.textbbox((0, 0), line, font=font)[
-                                3] for line in wrapped_text)
-
-        # Add padding to the image dimensions
-        width = max_line_width + 2 * self.padding
-        height = total_text_height + 2 * self.padding + len(wrapped_text) * 5
-
-        # Create a new image with transparent background
-        image = Image.new('RGBA', (width, height), (0, 0, 0, 0))
-        draw = ImageDraw.Draw(image)
-
-        # Draw the wrapped text on the image with shadow for better visibility
-        y_position = self.padding
-        for line in wrapped_text:
-            # Shadow
-            draw.text((self.padding + 2, y_position + 2), line,
-                      fill='gray', font=font)  # Shadow in gray
-            # Main text
-            draw.text((self.padding, y_position), line,
-                      fill='black', font=font)  # Main black text
-            # Add padding between lines
-            y_position += draw.textbbox((0, 0), line, font=font)[3] + 5
-
-        # Save the image with transparent background
-        image.save(output_path, "PNG")
-        print(f"Image saved to {output_path}")
-
-    def create_image_with_text(self, text, output_path='images/output_image.png'):
+    def create_image_with_text(self, text, output_path='images/output_image.png', font_style="arial.ttf", transparent=False):
         # Initialize ImageDraw to determine text size
         # You may need to adjust the font path
-        font = ImageFont.truetype("arial.ttf", self.font_size)
+        font = ImageFont.truetype(font_style, self.font_size)
         # Create a dummy image to get text size
-        dummy_image = Image.new('RGB', (1, 1))
+        if transparent:
+            dummy_image = Image.new('RGBA', (1, 1), (0, 0, 0, 0))
+        else:
+            dummy_image = Image.new('RGB', (1, 1))
         draw = ImageDraw.Draw(dummy_image)
 
         # Define text wrapping function
@@ -91,16 +43,23 @@ class SubtitlesGenerator:
         height = total_text_height + 2 * self.padding + len(wrapped_text)*5
 
         # Create a new image with calculated dimensions
-        image = Image.new('RGB', (width, height), 'white')
+        if transparent:
+            image = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+        else:
+            image = Image.new('RGB', (width, height), 'white')
         draw = ImageDraw.Draw(image)
 
-        # Draw the wrapped text on the image
+        # Draw the wrapped text on the image with shadow for better visibility
         y_position = self.padding
         for line in wrapped_text:
-            draw.text((self.padding, y_position),
-                      line, fill='black', font=font)
-            y_position += draw.textbbox((0, 0), line, font=font)[3] - draw.textbbox(
-                (0, 0), line, font=font)[1] + 5  # Adding some padding between lines
+            # Shadow
+            draw.text((self.padding + 2, y_position + 2), line,
+                      fill='gray', font=font)  # Shadow in gray
+            # Main text
+            draw.text((self.padding, y_position), line,
+                      fill='black', font=font)  # Main black text
+            # Add padding between lines
+            y_position += draw.textbbox((0, 0), line, font=font)[3] + 5
 
         # Save the image
         image.save(output_path)
